17606174cac1b2ca05799d0458cd892bf6103cf8
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2024-08-30 00:15:07 +0300

    add solves

--- task2.js
+++ task2.js
@@ -5,3 +5,21 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const input = document.querySelector('#userNameInput');
+const button = document.querySelector('#getUserButton');
+const userCity = document.querySelector('#userCity');
+
+button.addEventListener('click', getCity);
+
+async function getCity() {
+  const inputValue = input.value;
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const users = await response.json();
+  console.log(users)
+  users.forEach(user => {
+    if (user.name === inputValue) {
+      userCity.innerHTML = `<p>${user.address.city}</p>`
+    }
+  });
+}
